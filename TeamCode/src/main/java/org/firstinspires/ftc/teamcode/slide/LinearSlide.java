b687bbb5bd39362c8107a0d5e1eeca3d883eee7b
Cryptotundra <buddybuster8@gmail.com>
2025-01-29 16:21:57 -0500

    Merged Part of IntoTheDeep_10785 for the new bot into code, minor work on other stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slide/LinearSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slide/LinearSlide.java
@@ -1,18 +1,21 @@
-package org.firstinspires.ftc.teamcode.slide;
+package org.firstinspires.ftc.teamcode.Slide;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
 
 
 public class LinearSlide {
 
     // Slide power parameters
-
+    public static double powerMin = 0;
+    public static double power = 0.1;
+    public static double powerMax = 1;
 
     // Slide height parameters
-    public static double powerMin;
-    public static double power;
-    public static double powerMax;
+    public static int heightMin = 0;
+    public static int heightMax = 0;
 
     // Sets up motors
     public DcMotorEx slideMotorL;
@@ -25,9 +28,22 @@ public class LinearSlide {
 
         // Initialization
         slideMotorL.setDirection(DcMotorEx.Direction.FORWARD);
-        slideMotorL.setDirection(DcMotorEx.Direction.REVERSE);
-
+        slideMotorR.setDirection(DcMotorEx.Direction.REVERSE);
+        slideMotorL.setTargetPosition();
+        slideMotorR.setTargetPosition();
+        slideMotorL.setPower();
+        slideMotorR.setPower();
+        slideMotorL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        slideMotorR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         slideMotorL.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         slideMotorR.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        slideMotorL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slideMotorR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
+
+    public double setSlide(int steps) {
+        slideMotorSteps = Range.clip(steps, heightMin, heightMax);
+        return slideMotorSteps;
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleopArya.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.util.Constants;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.pedropathing.pathgen.BezierCurve;
+import com.pedropathing.pathgen.PathChain;
+import com.pedropathing.pathgen.Point;
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import pedroPathing.constants.FConstants;
+import pedroPathing.constants.LConstants;
+import subsystem.numbers.PID;
+
+@Config
+@TeleOp (name = "v2 Teleop")
+public class teleopArya extends OpMode{
+    //Initialize Variables
+    //Pedro Pathing Follower
+    private Follower follower;
+    private final Pose startPose= new Pose(0, 0,0);
+
+    //Outtake Lift PID
+    private DcMotor liftMotor;
+    public PID liftPID = new PID(0.1,0,0);
+
+    //
+
+    @Override
+    public void init(){
+        // Initialize Follower
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(startPose);
+        //Initialize hardwareMap
+        liftMotor=hardwareMap.get(DcMotor.class, "lift");
+    }
+
+    @Override
+    public void start(){
+        follower.startTeleopDrive();
+    }
+
+    @Override
+    public void loop(){
+        //Information
+
+        //Drive
+        //drive();
+        //Intake
+        //Outtake
+        outtake();
+    }
+
+    public void drive(){
+
+        follower.setTeleOpMovementVectors(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x, false);
+        follower.update();
+
+        /* Telemetry Outputs of our Follower */
+        telemetry.addData("X", follower.getPose().getX());
+        telemetry.addData("Y", follower.getPose().getY());
+        telemetry.addData("Heading in Degrees", Math.toDegrees(follower.getPose().getHeading()));
+
+        /* Update Telemetry to the Driver Hub */
+        telemetry.update();
+    }
+
+    public void outtake(){
+        //Outtake Components:
+        //Slide Motor x2
+        //Slide Servo x1
+        //Slide Claw x1
+        
+    }
+}
